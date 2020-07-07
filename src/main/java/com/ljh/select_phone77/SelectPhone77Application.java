77acd458132334730f40876415105c37c3592e2f
JiahuiLiu2016 <JiahuiLiu2016@gmail.com>
2020-07-07 17:08:11 +0800

    初始版本

---
+++ src/main/java/com/ljh/select_phone77/SelectPhone77Application.java
@@ -0,0 +1,13 @@
+package com.ljh.select_phone77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectPhone77Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectPhone77Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljh/select_phone77/SelectPhone77ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.select_phone77;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectPhone77ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
